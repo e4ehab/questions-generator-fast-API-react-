fa1e986fb251f9ee2ec0e4e1841e651dfcc2045b
e4ehab <e4ehap@gmail.com>
2025-06-11 10:21:50 +0300

    create components and routes
    added new components and pages and created routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import { ClerkProviderWithRoutes } from "./auth/ClerkProviderWithRoutes.jsx"
 import { Route, Routes } from 'react-router-dom'
+import { layout } from './layout/Layout.jsx'
+import { ChallengeGenerator } from './challenge/ChallengeGenerator.jsx'
+import { HistoryPanel } from './history/HistoryPanel.jsx'
+import { AuthenticationPage } from './auth/AuthenticationPage.jsx'
 import './App.css'
 
 function App() {
@@ -8,8 +12,18 @@ function App() {
 
   return
   <ClerkProviderWithRoutes>
-    <Routes></Routes>
+    <Routes>
+      <Route path="/sign-in/*" element={<AuthenticationPage />} />
+      <Route path="/sign-up" element={<AuthenticationPage />} />
+
+      <Route element={<layout />}>
+        <Route path="/" element={<ChallengeGenerator />} />
+        <Route path="/history" element={<HistoryPanel />} />
+      </Route>
+
+    </Routes>
   </ClerkProviderWithRoutes>
 }
 
 export default App
+//if you want to go sign in page then render AuthenticationPage.jssx
