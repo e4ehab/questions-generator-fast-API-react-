c795681fc0a5f047b1479c4f55ce08c2557e4921
e4ehab <e4ehap@gmail.com>
2025-06-12 05:35:47 +0300

    navbar styling

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,5 +1,35 @@
 import "react"
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
+//userbutton -> press to see my profile
+import { Outlet, Navigate, Link } from "react-router-dom"
 
-export function Layout(){
-    return <></>
+export function Layout() {
+    return (
+        <div className="app-layout">
+            <header className="app-header">
+                <div className="header-content">
+                    <h1>Code Challenge Generator</h1>
+                    <nav>
+                        <SignedIn> {/* show the following if we are only signed-in */}
+                            <Link to="/">Generate Challenge</Link>
+                            <Link to="/history">History</Link>
+                            <UserButton />
+                        </SignedIn>
+
+                    </nav>
+                </div>
+            </header>
+
+            <main className="app-main">
+                <SignedOut>
+                    <Navigate to="/sign-in" replace />
+                </SignedOut>
+                <SignedIn>
+                    <Outlet />
+                </SignedIn>
+            </main>
+
+
+        </div>
+    );
 }
