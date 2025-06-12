d41b55eb241f4ef509e1229f2e8ca2ebe876133e
e4ehab <e4ehap@gmail.com>
2025-06-13 02:23:46 +0300

    update app.py
    setup fastapi and origins and adding clerk_secret_key
    later will edit the origins to other paths

---
+++ backend/src/app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+#CORSMiddleware -> allow the frontend to send requests to backend
+from clerk_backend_api import Clerk
+import os
+
+clerk_sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
+# tell clerk that we are the owners of the application because we have the secret key ,
+# so we can do the tooken operations
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  #allow orgins -> means allow any one to send requests to the backend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
